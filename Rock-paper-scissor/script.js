5aef9b234e57ceb4b339fc1c973da4b75ef1acb7
sulaiman adamu <adamusulaiman2021@gmail.com>
2024-10-25 13:35:21 +0100

    check user input and compare it to computer guess  then increase the winners score.

--- Rock-paper-scissor/script.js
+++ Rock-paper-scissor/script.js
@@ -1,4 +1,6 @@
-const options = ["Scissors", "Rock", "Paper"]
+const options = ["SCISSORS", "ROCK", "PAPER"]
+let humanScore = 0
+let computerScore = 0
 
 function getRandomInt(max){
     return Math.floor(Math.random() * max)
@@ -13,6 +15,43 @@ function getComputerChoice(arraySize){
 function getHumanChoice(){
    let choice = prompt("Rock Paper Scissors? ")
    return choice
+}
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice === computerChoice){
+        return "Wow, its a tie"
+    }
+
+    else if(humanChoice === "ROCK" && computerChoice === "SCISSORS"){
+        humanScore += 1
+        return "You win, Rock beats scissors anyday anytime."
+    }
+
+    else if(computerChoice === "SCISSORS" && humanChoice ==="PAPER"){
+        humanScore += 1
+         return "You win, Scissors beats Paper anyday anytime."
+    }
+
+    else if(computerChoice === "PAPER" && humanChoice ==="ROCK"){
+        humanScore += 1
+        return "You win, Paper beats Rock anyday anytime"
+    }
+
+    else if( computerChoice === "ROCK" && humanChoice === "SCISSORS"){
+         computerScore += 1
+        return "Computer Won!"
+    }
+
+    else if(humanChoice === "SCISSORS" &&  computerChoice ==="PAPER"){
+         computerScore += 1
+        return "Computer Won!"
+    }
+
+    else if(humanChoice === "PAPER" && computerChoice ==="ROCK"){
+        computerScore += 1
+        return "Computer Won!"
+    }
+
 }
 
 console.log(getComputerChoice(options.length))
