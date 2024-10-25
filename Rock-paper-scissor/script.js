7b3eb6ebac5a6d0a18aabc4cc7033d465e23d3df
sulaiman adamu <adamusulaiman2021@gmail.com>
2024-10-25 10:19:35 +0100

    return human choice in the game and the computer choice

--- Rock-paper-scissor/script.js
+++ Rock-paper-scissor/script.js
@@ -1,1 +1,20 @@
+const options = ["Scissors", "Rock", "Paper"]
+
+function getRandomInt(max){
+    return Math.floor(Math.random() * max)
+}
+
+//select a member of the array as computer choice
+function getComputerChoice(arraySize){
+    return options[getRandomInt(arraySize)]
+}
+
+//select a member of the array as human choice
+function getHumanChoice(arraySize){
+    return options[getRandomInt(arraySize)]
+}
+
+console.log(getComputerChoice(options.length))
+console.log(getHumanChoice(options.length))
+
 console.log("hello world!")
