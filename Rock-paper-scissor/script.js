d7a8d71821f27fbf68a2e3e64c4cd43e5a804278
sulaiman adamu <adamusulaiman2021@gmail.com>
2025-01-26 09:27:26 +0100

    added the spinning mechanism to the game where computer spin choices, until it settles for one. added a feature, that removes all other signs except the one selected by the human player. added a css style to rotate the game orientation based on the screen size.

--- Rock-paper-scissor/script.js
+++ Rock-paper-scissor/script.js
@@ -1,23 +1,58 @@
-const options = ["SCISSORS", "ROCK", "PAPER"]
+const options = [{"SCISSORS": './images/scissors.png'}, {"ROCK":'./images/rock.png'}, {"PAPER":'./images/paper.png'}]
+const computerChoiceImg = document.querySelector('#computerChoiceimg')
+const dialogBox = document.querySelector('#dialog-box')
+const btns = document.querySelector('#btns')
+let computerChoiceSym = ''
+let humanChoice = ''
+
+//initial computer image befor the spin.
+computerChoiceImg.src = Object.values(options[getRandomInt(options.length)])
+
+
+btns.addEventListener('click', getHumanChoice)
+
+
+function getHumanChoice(e){
+    humanChoice = e.target.getAttribute('data-represent')
+    btns.removeEventListener('click', getHumanChoice)
+    removeUnselectedSigns(e.target.parentNode, this)
+    console.log(humanChoice)
+}
+
+function removeUnselectedSigns(selected, element){
+    element.textContent = ''
+    element.appendChild(selected)
+}
+
+
+function randomImages(){
+const intervalId = setInterval(randomImages, 500)
+setTimeout(() => {
+  clearInterval(intervalId); 
+}, 10000);
+}
+
+function getComputerChoice(){
+        const entries = Object.entries(options[getRandomInt(options.length)])
+        const [computerChoice, src] = entries[0]
+        computerChoiceImg.src = src
+        computerChoiceSym = computerChoice
+}
+
+
+
+
+
 let humanScore = 0
 let computerScore = 0
 let error = 0
+
 
 
 function getRandomInt(max){
     return Math.floor(Math.random() * max)
 }
 
-//select a member of the array as computer choice
-function getComputerChoice(arraySize){
-    return options[getRandomInt(arraySize)]
-}
-
-//accept input from users 
-function getHumanChoice(){
-    let choice = prompt("Rock,  Paper or Scissors? ")
-    return choice.toUpperCase();
-}
 
 function playRound(humanChoice, computerChoice){
     if(humanChoice === computerChoice){
@@ -58,14 +93,14 @@ function playRound(humanChoice, computerChoice){
 
 
 // allow us to repeat playing of the game.
-function playGame(){
-        // computerChoice = getComputerChoice(options.length)
-        // humanChoice = getHumanChoice()
+// function playGame(){
+//         // computerChoice = getComputerChoice(options.length)
+//         // humanChoice = getHumanChoice()
 
-        console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        playRound(humanChoice, computerChoice)
-        console.log("Computer: ", computerScore,"     ", "Your Score: ", humanScore)
-        console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-}
+//         console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+//         playRound(humanChoice, computerChoice)
+//         console.log("Computer: ", computerScore,"     ", "Your Score: ", humanScore)
+//         console.log("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+// }
 
-playGame()
+// playGame()
